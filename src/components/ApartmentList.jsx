12099f71696c8cbeb9757184020cdd8ab7c68d7c
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-17 16:37:25 +0100

    Apartmentlist fetching Data and displaying cards

--- src/components/ApartmentList.jsx
+++ src/components/ApartmentList.jsx
@@ -1,25 +1,70 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ApartmentList() {
-  const {cityId} = useParams();
+  const url = import.meta.env.VITE_API_URL;
+  const { cityId } = useParams();
+  const [allApartments, setAllApartments] = useState(null);
+  const [apartmentsInCity, setApartmentsInCity] = useState(null);
+
+  const fetchApartments = () => {
+    axios
+      .get(`${url}/apartments`)
+      .then((res) => {
+        setAllApartments(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchApartments();
+  }, []);
+
+  // wait for allApartments be filled with apartmentlist from fetching
+  useEffect(() => {
+    if (allApartments) {
+      const newApartmentsArr = allApartments.filter((apartment) => {
+        return apartment.cityId == cityId;
+      });
+      setApartmentsInCity(newApartmentsArr);
+      console.log(apartmentsInCity);
+    }
+  }, [allApartments]);
+
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img
-        className="card-img-top"
-        src="https://cdn.britannica.com/06/171306-050-C88DD752/Aurora-borealis-peninsula-Snaefellsnes-Iceland-March-2013.jpg"
-        alt="Card image cap"
-      />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
-    </div>
+    <>
+      (
+      {apartmentsInCity &&
+        apartmentsInCity.map((apartment) => {
+          return (
+            <div className="card" style={{ width: "18rem" }} key={apartment.id}>
+              <img
+                className="card-img-top"
+                src="https://cdn.britannica.com/06/171306-050-C88DD752/Aurora-borealis-peninsula-Snaefellsnes-Iceland-March-2013.jpg"
+                alt="Card image cap"
+              />
+              <div className="card-body">
+                <div>
+                  <h5 className="card-title">{apartment.title}</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus, quae blanditiis tempora
+                  </p>
+                </div>
+
+                <p className="card-text">
+                  {apartment.pricePerNight} € per Night
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Check it out
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      )
+    </>
   );
 }
 
