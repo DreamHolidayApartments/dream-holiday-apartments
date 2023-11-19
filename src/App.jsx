990dafeebd2fdbc647e2e14ec446b7509958ce10
Vidisha Shah <shahvidisha1996@gmail.com>
2023-11-19 15:32:50 +0100

    Navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ApartmentList from "./pages/ApartmentList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+    {
+      location.pathname !== '/' &&  <Navbar/>
+    }
+    
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/apartmentList/:cityId" element={<ApartmentList />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
   return (
-    <div>
-      
+  <nav className="navBarStyle">
+    <div className="nav1">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="#">About</Link>
+        </li>
+        <li>
+          <Link to="#">Contact us</Link>
+        </li>
+      </ul>
     </div>
+    <div className="nav2">
+    <button class="btn btn-outline-light" type="submit">Add Apartment</button>
+    </div>
+  </nav>
   )
 }
 
