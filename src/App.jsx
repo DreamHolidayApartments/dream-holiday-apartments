474463b941e5499d0221864c0a873aaecdeddbbf
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-20 15:08:29 +0100

    Fetching data in App.jsx and pass it as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
 import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import HomePage from "./pages/HomePage";
 import ApartmentList from "./pages/ApartmentList";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 function App() {
   const location = useLocation();
+  let url = import.meta.env.VITE_API_URL;
+  const [countries, setCountries] = useState(null);
+  const [cities, setCities] = useState(null);
+
+  const fetchData = (param, setFun) => {
+    axios
+      .get(`${url}/${param}`)
+      .then((res) => {
+        setFun(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData("countries", setCountries);
+    fetchData("cities", setCities);
+  }, []);
+
+  useEffect(() => {
+    console.log(cities);
+  }, [cities]);
 
   return (
     <div className="App">
       {location.pathname !== "/" && <Navbar />}
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        (cities && countries &&{" "}
+        <Route
+          path="/"
+          element={
+            <HomePage fetchedCountries={countries} fetchedCities={cities} />
+          }
+        />
+        )
         <Route path="/apartmentList/:cityId" element={<ApartmentList />} />
       </Routes>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,90 +1,70 @@
 import axios from "axios";
-import "../styles/HomePage.css"
+import "../styles/HomePage.css";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function HomePage() {
+function HomePage({ fetchedCountries, fetchedCities }) {
   let url = import.meta.env.VITE_API_URL;
-  const [countries, setCountries] = useState(null);
-  const [cities, setCities] = useState(null);
+
   const [filteredCities, setFilteredCities] = useState(null);
   const [selectedCity, setSelectedCity] = useState(null);
 
-  const getCountriesData = () => {
-    axios
-      .get(`${url}/countries`)
-      .then((res) => {
-        console.log(res.data);
-        setCountries(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const getCitiesData = () => {
-    axios
-      .get(`${url}/cities`)
-      .then((res) => {
-        setCities(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   const filterCities = (id) => {
-    let newList = cities.filter((city) => city.countryId == id);
+    let newList = fetchedCities.filter((city) => city.countryId == id);
     setFilteredCities(newList);
   };
-
-  useEffect(() => {
-    getCountriesData();
-    getCitiesData();
-  }, []);
 
   const handleSelectCountry = (e) => {
     filterCities(e.target.value);
     console.log(e.target.value);
   };
 
-
-
   return (
     <div className="HomePage">
-     <div className="homepage-ele">
-      <label className="countryLabel">
-        <select
-          className="custom-select"
-          id="inputGroupSelect01"
-          onChange={handleSelectCountry}
-        >
-          <option>Select Country</option>
-          {countries &&
-            countries.map((country) => {
-              return <option value={country.id} key={country.id}>{country.name}</option>;
-            })}
-        </select>
-      </label>
+      <div className="homepage-ele">
+        <label className="countryLabel">
+          <select
+            className="custom-select"
+            id="inputGroupSelect01"
+            onChange={handleSelectCountry}
+          >
+            <option>Select Country</option>
+            {fetchedCountries &&
+              fetchedCountries.map((country) => {
+                return (
+                  <option value={country.id} key={country.id}>
+                    {country.name}
+                  </option>
+                );
+              })}
+          </select>
+        </label>
 
-      <label className="cityLabel">
-        <select
-          className="custom-select"
-          id="inputGroupSelect02"
-          onChange={(e)=>{setSelectedCity(e.target.value)}}
-        >
-          <option>Select City</option>
-          {filteredCities &&
-            filteredCities.map((city) => {
-              return <option value={city.id} key={city.id}>{city.name}</option>;
-            })}
-        </select>
-      </label>
-      {
-        selectedCity && (<Link id="btnSubmit1" to = {`/apartmentList/${selectedCity}`}>Submit</Link>)
-      }
+        <label className="cityLabel">
+          <select
+            className="custom-select"
+            id="inputGroupSelect02"
+            onChange={(e) => {
+              setSelectedCity(e.target.value);
+            }}
+          >
+            <option>Select City</option>
+            {filteredCities &&
+              filteredCities.map((city) => {
+                return (
+                  <option value={city.id} key={city.id}>
+                    {city.name}
+                  </option>
+                );
+              })}
+          </select>
+        </label>
+        {selectedCity && (
+          <Link id="btnSubmit1" to={`/apartmentList/${selectedCity}`}>
+            Submit
+          </Link>
+        )}
       </div>
-
     </div>
   );
 }
