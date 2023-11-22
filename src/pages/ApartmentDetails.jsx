238f6bedd5747d1a48a531edde4cac3968b67288
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-22 16:47:03 +0100

    add placeholder img to apt details

--- src/pages/ApartmentDetails.jsx
+++ src/pages/ApartmentDetails.jsx
@@ -6,11 +6,12 @@ import ModalEdit from "../components/ModalEdit";
 import editSvg from "../assets/pencil-simple-line.svg";
 import deleteSvg from "../assets/trash.svg";
 import ModalDelete from "../components/ModalDelete";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import placeholderImg from "../assets/placeholder-img.png";
 
 function ApartmentDetails({ fetchedData, setApartments }) {
   const [showDelete, setShowDelete] = useState(false);
@@ -44,52 +45,70 @@ function ApartmentDetails({ fetchedData, setApartments }) {
     <div>
       {apartmentDetails && (
         <>
-        <Container style={{ width: "100rem",margin:"0 auto" }}>
-          <Row style={{ margin:"40px 0" }}>
+          <Container style={{ width: "100rem", margin: "0 auto" }}>
+            <Row style={{ margin: "40px 0" }}>
+              <Col>
+                <Card style={{ width: "40rem", height: "30rem" }}>
+                  {apartmentDetails.pictureURL ? (
+                    <Card.Img
+                      style={{ width: "40rem", height: "30rem" }}
+                      src={apartmentDetails.pictureURL}
+                    />
+                  ) : (
+                    <Card.Img
+                      style={{ width: "40rem", height: "30rem" }}
+                      src={placeholderImg}
+                    />
+                  )}
+                </Card>
+              </Col>
 
-            <Col>
-              <Card style={{ width: "40rem", height: "30rem" }}>
-                <Card.Img style={{ width: "40rem", height: "30rem" }}  src={apartmentDetails.pictureURL} />
-              </Card>
-            </Col>
+              <Col>
+                <Card
+                  style={{
+                    width: "40rem",
+                    height: "30rem",
+                    backgroundColor: "aliceblue",
+                  }}
+                >
+                  <Card.Body>
+                    <Card.Title style={{ fontSize: "40px" }}>
+                      {apartmentDetails.title}
+                    </Card.Title>
+                    <Card.Text className="cardText">
+                      {apartmentDetails.address}
+                    </Card.Text>
+                    <hr />
+                    {apartmentDetails.book === true ? (
+                      <Card.Title className="bookAval">
+                        Booking is Available
+                      </Card.Title>
+                    ) : (
+                      <Card.Title className="bookAval">
+                        Booking is Not Available
+                      </Card.Title>
+                    )}
+                    <Card.Text style={{ fontSize: "20px", textAlign: "end" }}>
+                      &#9733;{apartmentDetails.rating}
+                    </Card.Text>
+                    <Card.Title>Description</Card.Title>
+                    <Card.Text>{apartmentDetails.description}</Card.Text>
+                    <Card.Title>Guests</Card.Title>
+                    <Card.Text>{apartmentDetails.numOfGuest} Beds</Card.Text>
+                    <Card.Title>Price per Night</Card.Title>
+                    <Card.Text>{apartmentDetails.pricePerNight} €</Card.Text>
 
-            <Col>
-              <Card style={{ width: "40rem", height: "30rem", backgroundColor: "aliceblue" }}>
-                <Card.Body >
-                  <Card.Title style={{ fontSize: "40px" }}>{apartmentDetails.title}</Card.Title>
-                  <Card.Text className="cardText">
-                    {apartmentDetails.address}
-                  </Card.Text>
-                  <hr/>
-                  {
-                    apartmentDetails.book === true 
-                    ? <Card.Title className = "bookAval">Booking is Available</Card.Title> 
-                    : <Card.Title className = "bookAval">Booking is Not Available</Card.Title>
-                  }
-                  <Card.Text style={{ fontSize: "20px" , textAlign: "end"}} >
-                    &#9733;{apartmentDetails.rating}
-                  </Card.Text>
-                  <Card.Title>Description</Card.Title>
-                  <Card.Text>
-                    {apartmentDetails.description}
-                  </Card.Text>
-                  <Card.Title>Guests</Card.Title>
-                  <Card.Text>
-                    {apartmentDetails.numOfGuest} Beds
-                  </Card.Text>
-                  <Card.Title>Price per Night</Card.Title>
-                  <Card.Text>
-                    {apartmentDetails.pricePerNight} €
-                  </Card.Text>
-              
-                  <Link className="cardBtn" to={`/apartment-list/${apartmentDetails.cityId}`}>Go Back</Link>
-                 
-                </Card.Body>
-              </Card>
-            </Col>
-
-          </Row>
-</Container>
+                    <Link
+                      className="cardBtn"
+                      to={`/apartment-list/${apartmentDetails.cityId}`}
+                    >
+                      Go Back
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
 
           <ModalEdit
             show={show}
